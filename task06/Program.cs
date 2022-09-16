77bfc0362bcb5560d0317dad8585bc9d1cadfea5
Kate <ktvlphoto@outlook.com>
2022-09-16 10:40:33 +0300

    task06

---
+++ task06/Program.cs
@@ -0,0 +1,19 @@
+﻿// напишите программу, которая на вход
+// принимает число и выдает,
+// является ли оно четным
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+
+    {
+        Console.WriteLine(number + " - Четное число: ");
+    }
+
+    else
+    
+    {
+        Console.WriteLine(number + " - Нечетное число");
+    }
