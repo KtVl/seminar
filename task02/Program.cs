6062d8c07be8110506711167d34cbc4ef0f2c70a
Kate <ktvlphoto@outlook.com>
2022-09-16 09:32:43 +0300

    исправлены ошибки в задаче

--- task02/Program.cs
+++ task02/Program.cs
@@ -14,13 +14,13 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 > num2)
 
 {
-    Console.WriteLine(num1 + " > " + num2);
+    Console.WriteLine( " наибольшее число " + num1);
 }
 
 else if (num2 > num1)
 
 {
-    Console.WriteLine(num2 + " > " + num1);
+    Console.WriteLine(" наибольшее число " + num2);
 }
 
 else
