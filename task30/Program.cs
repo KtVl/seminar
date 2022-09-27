40b3cb7efe40471a4b6bf4dde00900a20e6732f3
Kate <ktvlphoto@outlook.com>
2022-09-27 12:02:35 +0300

    task30

--- task30/Program.cs
+++ task30/Program.cs
@@ -10,19 +10,19 @@ PrintArray(array);
 
 void GetArray(int[] arr)
 {
-int length = arr.Length;
-Random rnd = new Random();
-for (int i = 0; i < length; i++)
-{
-arr[i] = rnd.Next(0, 2);
-}
+    int length = arr.Length;
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
 }
 void PrintArray(int[] array)
 {
-int count = array.Length;
-for (int i = 0; i < count; i++)
-{
-Console.Write($"{array[i]} ");
-}
-Console.WriteLine();
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
