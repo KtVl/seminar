71e59208795d06e1b57dfad36ea9dc3957f99c20
Kate <ktvlphoto@outlook.com>
2022-09-30 13:04:37 +0300

    task38

---
+++ task38/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 38:
+// Задайте массив вещественных чисел.
+//  Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+
+
+double[] numbers = new double[5];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+    if (numbers[x] > max)
+        {
+            max = numbers[x];
+        }
+    if (numbers[x] < min)
+        {
+            min = numbers[x];
+        }
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(1,100)) / 10;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
